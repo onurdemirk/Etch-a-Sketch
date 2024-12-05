476929301298e0acbf8c342aed93b50272000cca
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2024-12-05 01:58:12 +0100

    Implement dynamic functionality for project
    - Added dynamic grid creation with customizable size.
    - Integrated color selection via radio buttons for grid box hover effects.
    - Implemented input validation for grid size and displayed error messages for invalid values.
    - Added a reset functionality using the clear button to revert grid to default size.
    - Enhanced interactivity with smooth hover effects for grid boxes.

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-let mainBlock = () => {
+let blockNumber = 16;
+let currentColor = "black";
 
-    const container = document.querySelector(".container");
+const gridInput = document.querySelector("#gridSize");
+const submit = document.querySelector("#gridSizeSubmit");
+const alert = document.querySelector("#alertMessage");
+const clear = document.querySelector("#clear");
+const blue = document.querySelector("#blue");
+const black = document.querySelector("#black");
+const red = document.querySelector("#red");
+const eraser = document.querySelector("#eraser");
 
-    for (let i = 0; i < 16; i++) {
-        const blocks = document.createElement("div");
-        blocks.style.border = "0px solid grey";
-        blocks.classList.add("blocks");
+let createBlock = (blockNumber) => {
+  const container = document.querySelector(".container");
 
-        container.appendChild(blocks);
+  container.innerHTML = "";
 
-        for (let j = 0; j < 16; j++) {
-            const box = document.createElement("div");
-            box.classList.add("box");
-    
-            blocks.appendChild(box);
-        }
+  for (let i = 0; i < blockNumber; i++) {
+    const blocks = document.createElement("div");
+    blocks.style.border = "0px solid grey";
+    blocks.classList.add("blocks");
+
+    container.appendChild(blocks);
+
+    for (let j = 0; j < blockNumber; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+
+      box.addEventListener("mouseenter", () => {
+        box.style.backgroundColor = currentColor;
+      });
+
+      blocks.appendChild(box);
     }
-    }
-  
+  }
+};
 
-mainBlock();
+createBlock(blockNumber);
+
+submit.addEventListener("click", () => {
+  if (parseInt(gridInput.value) >= 2 && parseInt(gridInput.value) <= 99) {
+    alert.textContent = "";
+    createBlock(gridInput.value);
+  } else {
+    alert.textContent = "Enter a number between 2 and 99";
+  }
+});
+
+clear.addEventListener("click", () => {
+  alert.textContent = "";
+  createBlock(16);
+});
+
+let getColor = document.querySelectorAll('input[name="button"]');
+getColor.forEach((radio) => {
+  radio.addEventListener("change", (e) => {
+    currentColor = e.target.value;
+  });
+});
